572c1a39288c7bd51722820e0050014ca82cfbbb
Stephanie Frias <stephfz@gmail.com>
2025-10-21 16:37:58 -0300

    page objects v1

---
+++ page-objects/navigationPage.ts
@@ -0,0 +1,50 @@
+import { Locator, Page } from "@playwright/test";
+
+export class NavigationPage{
+    readonly page: Page
+    readonly formLayoutMenuItem: Locator
+    readonly datePickerMenuItem: Locator
+    readonly toastrMenuItem: Locator
+    readonly smartTableMenuItem: Locator
+
+    constructor(page :Page){
+        this.page = page
+        this.formLayoutMenuItem = page.getByText('Form Layouts');
+        this.datePickerMenuItem = page.getByText('Datepicker');
+        this.smartTableMenuItem = page.getByText('Smart Table');
+        this.toastrMenuItem = page.getByText('Toastr');
+    }
+
+    async formLayoutsPage(){
+        await this.selectGroupMenuItem('Forms')
+        await this.formLayoutMenuItem.click()  
+    }
+
+    async datePickerPage(){
+        await this.selectGroupMenuItem('Forms')
+        //await this.page.waitForTimeout(1000)
+        await this.datePickerMenuItem.click()
+
+    }
+
+    async smartTablePage(){
+       await this.selectGroupMenuItem('Tables & Data')
+        await this.smartTableMenuItem.click()
+
+    }
+
+    async toastrPage(){
+       await this.selectGroupMenuItem('Modal & Overlays')
+        await this.toastrMenuItem.click()
+
+    }
+
+    private async selectGroupMenuItem(groupItemTitle: string){
+        const groupMenutItem = this.page.getByTitle(groupItemTitle);
+        const expandedState = await groupMenutItem.getAttribute('aria-expanded')
+        if (expandedState == "false"){
+            await groupMenutItem.click()
+        }
+    }
+
+}
