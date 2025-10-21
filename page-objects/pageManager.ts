97dc6f4fc4e2a73031bfd90bafaa13b5a02e5a24
Stephanie Frias <stephfz@gmail.com>
2025-10-21 17:48:55 -0300

    Adding Page Manager Class

---
+++ page-objects/pageManager.ts
@@ -0,0 +1,30 @@
+import { Page, test } from '@playwright/test';
+import { NavigationPage } from '../page-objects/navigationPage';
+import { FormLayoutsPage } from '../page-objects/formLayoutsPage';
+import { DatePickerPage } from '../page-objects/datePickerPage';
+
+export class PageManager{
+    private readonly page:Page
+    private readonly navigationPage: NavigationPage;
+    private readonly formsLayoutPage: FormLayoutsPage;
+    private readonly datePickerPage: DatePickerPage;
+
+    constructor(page: Page){
+        this.page = page
+        this.navigationPage = new NavigationPage(this.page)
+        this.formsLayoutPage = new FormLayoutsPage(this.page)
+        this.datePickerPage = new DatePickerPage(this.page)
+    }
+
+    navigateTo(){
+        return this.navigationPage
+    }
+
+    onFormsLayoutPage(){
+        return this.formsLayoutPage
+    }
+
+    onDatePickerPage(){
+        return this.datePickerPage
+    }
+}
--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,32 +1,27 @@
 import { expect, test } from '@playwright/test';
-import { NavigationPage } from '../page-objects/navigationPage';
-import { FormLayoutsPage } from '../page-objects/formLayoutsPage';
-import { DatePickerPage } from '../page-objects/datePickerPage';
+import { PageManager } from '../page-objects/pageManager';
 
 test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200/');
 })
 
 test('navigate to form page', async({page}) => {
-    const navigateTo = new NavigationPage(page);
-    await navigateTo.formLayoutsPage()
-    await navigateTo.datePickerPage()
+    const pm = new PageManager(page)
+    await pm.navigateTo().formLayoutsPage()
+    await pm.navigateTo().datePickerPage()
 })
 
 
 test('parametrized methods', async({page}) => {
-    const navigateTo = new NavigationPage(page)
-    const onFormsLayoutPage = new FormLayoutsPage(page)
+    const pm = new PageManager(page)
 
-    await navigateTo.formLayoutsPage()
-    await onFormsLayoutPage.submitUsingTheGridFormWithCredentialsAndSelectionOption('steph@mail.com', '123', 'Option 1')
+    await pm.navigateTo().formLayoutsPage()
+    await pm.onFormsLayoutPage().submitUsingTheGridFormWithCredentialsAndSelectionOption('steph@mail.com', '123', 'Option 1')
+    await pm.onFormsLayoutPage().submitInlineFormWithEmailAndCheckbox('Steph', 'steph@mail.com', true)
 
-    await onFormsLayoutPage.submitInlineFormWithEmailAndCheckbox('Steph', 'steph@mail.com', true)
-
-    const onDatePickerPage = new DatePickerPage(page)
-    await navigateTo.datePickerPage()
-    await onDatePickerPage.selectCommonDatePickerDateFromToday(1)
-    await onDatePickerPage.selectDatepickerWithRangeFromToday(3,5)
+    await pm.navigateTo().datePickerPage()
+    await pm.onDatePickerPage().selectCommonDatePickerDateFromToday(1)
+    await pm.onDatePickerPage().selectDatepickerWithRangeFromToday(3,5)
 })
 
 
