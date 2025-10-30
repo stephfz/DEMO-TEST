e22f4cab3e552b8526381fed2c1bf6eb554cd4f5
Stephanie Frias <stephfz@gmail.com>
2025-10-30 15:03:05 -0300

    add Faker Library

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,5 +1,6 @@
 import { expect, test } from '@playwright/test';
 import { PageManager } from '../page-objects/pageManager';
+import { faker } from '@faker-js/faker'
 
 test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200/');
@@ -15,9 +16,12 @@ test('navigate to form page', async({page}) => {
 test('parametrized methods', async({page}) => {
     const pm = new PageManager(page)
 
+    const randomFullName = faker.person.fullName()
+    const randomEmail = `${randomFullName.replace(' ','')}${faker.number.int(100)}@test.com`
+
     await pm.navigateTo().formLayoutsPage()
     await pm.onFormsLayoutPage().submitUsingTheGridFormWithCredentialsAndSelectionOption('steph@mail.com', '123', 'Option 1')
-    await pm.onFormsLayoutPage().submitInlineFormWithEmailAndCheckbox('Steph', 'steph@mail.com', true)
+    await pm.onFormsLayoutPage().submitInlineFormWithEmailAndCheckbox(randomFullName, randomEmail, true)
 
     await pm.navigateTo().datePickerPage()
     await pm.onDatePickerPage().selectCommonDatePickerDateFromToday(1)
